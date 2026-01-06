8131a7ea4f0c4eb1316fc9314a71ffb33ae891d9
JEN023__ <lunar_halo_@naver.com>
2026-01-06 14:23:25 +0900

    dispatch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { createStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifies = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const countStore = createStore(countModifier);
+const countStore = createStore(countModifies);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
